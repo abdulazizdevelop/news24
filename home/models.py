894c835e6cfd77f3c9139922d0fc42229b1cb3c4
Abdulaziz <152422679+abdulazizdevelop@users.noreply.github.com>
2023-11-30 22:40:47 +0500

    Update models.py

--- home/models.py
+++ home/models.py
@@ -42,8 +42,8 @@ class News(BaseModel):
     view_count = models.BigIntegerField(default=0, blank=True, null=True)#2
     body = RichTextField() #3
     slug = models.SlugField() #4
-    user = models.ForeignKey( User ,on_delete= models.SET_NULL , null= True) #5
-    category = models.ForeignKey(Category ,on_delete=models.CASCADE, null= True , related_name = 'category') #6
+    user = models.ForeignKey( User , on_delete= models.SET_NULL , null= True) #5
+    category = models.ForeignKey(Category , on_delete=models.CASCADE , null= True , related_name = 'category') #6
     tags = models.ManyToManyField(Tags, related_name="tags") #7
     is_active = models.BooleanField(default= True)
     
